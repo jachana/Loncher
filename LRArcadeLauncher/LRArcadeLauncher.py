41a638ad5fb96a6e61840adf7daaa71b09afe79c
Jurgen Heysen <jheysen@gmail.com>
2013-09-01 23:29:16 -0400

    Agregado método para imprimir debug y un poco más de descripciones

--- LRArcadeLauncher/LRArcadeLauncher.py
+++ LRArcadeLauncher/LRArcadeLauncher.py
@@ -1,11 +1,20 @@
 import pygame
+#import Start
 
 class GameCaller:
     """clase que modela las llamadas a los juegos"""
     def __init__(self):
+        """Constuye un nuevo GameCaller"""
         #constructor
-        print("GameCaller Inicializado")
-        self.__DbgMsg = True #Activar o desactivar mensajes de debug
+        #Activar o desactivar mensajes de debug
+        self.__DbgMsg = True 
+        if self.__DbgMsg:
+            print("GameCaller Inicializado")
+
+    def DbgOut(self, Msg):
+        """Método que permite imprimir mensajes de debug a consola cuando se activan estos mensajes"""
+        if self.__DbgMsg:
+            print(Msg)
 
     def GameCall(self,gameEP,MTMode = False):
         """Método que realiza la llamada al Entrypoint del juego. gameEP debe ser una instancia de Start o derivado. MTMode es un valor booleano que indica si la llamada se realiza en un nuevo Thread o no (Default: Falso)"""
@@ -14,7 +23,10 @@ class GameCaller:
             if self.__DbgMsg: 
                 print("Llamada MT")
             try:
-                GameThead = pygame.threads.Thread(gameEP.Go).start()
+                GameThead = pygame.threads.Thread(None,gameEP.Go,"Juego",None,None,None)
+                GameThead.setDaemon(False)
+                GameThead.start()
+                GameThead.join()
                 return 0
             except:
                 return -1
