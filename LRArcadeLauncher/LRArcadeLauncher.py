e1dfb69c6fcafbb85c05d40cfeaf96391077ef93
Jurgen Heysen <jheysen@gmail.com>
2013-09-01 22:47:13 -0400

    Probablemente tenga loads para arreglar, pero creo que la cosa se vería mas o menos así

--- LRArcadeLauncher/LRArcadeLauncher.py
+++ LRArcadeLauncher/LRArcadeLauncher.py
@@ -1,1 +1,33 @@
-print('Hello World')
+import pygame
+
+class GameCaller:
+    """clase que modela las llamadas a los juegos"""
+    def __init__(self):
+        #constructor
+        print("GameCaller Inicializado")
+        __DbgMsg = True #Activar o desactivar mensajes de debug
+
+    def GameCall(self,gameEP,MTMode = False):
+        """Método que realiza la llamada al Entrypoint del juego. gameEP debe ser una instancia de Start o derivado. MTMode es un valor booleano que indica si la llamada se realiza en un nuevo Thread o no (Default: Falso)"""
+        if MTMode:
+            #Llamada MultiTheaded
+            if __DbgMsg: 
+                print("Llamada MT")
+            try:
+                GameThead = pygame.threads.Thread(gameEP.Go).start()
+                return 0
+            except:
+                return -1
+        else:
+            #Llamada dentro del mismo Thread
+            if __DbgMsg: 
+                print("Llamada ST")
+            ec = 0
+            try:
+                ec = gameEP.Go()
+                return ec
+            except:
+                if __DbgMsg: 
+                    print("Excepción en juego, retornando")
+                return -1
+
