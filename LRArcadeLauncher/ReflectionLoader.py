0fe955c5d44feda153ab3991b05db4e9f99074ef
julio <jachana@uc.cl>
2013-11-28 20:28:28 -0300

    Tenemos launch por reflection señores. Usar LaunchGameFromFileMT(...)

--- LRArcadeLauncher/ReflectionLoader.py
+++ LRArcadeLauncher/ReflectionLoader.py
@@ -1,11 +1,23 @@
 from LRArcadeLauncher import GameCaller
 import imp
+import StartBase
+import multiprocessing
 
 def LaunchGameFromFile(filename,classname,modulename):
-	loaded = imp.load_source(modulename,filename)
-	toLoad = eval("loaded."+classname)()
-	print(toLoad.Go)
-	print(loaded)
-	print(toLoad)
-	caller = GameCaller()
-	caller.GameCall(toLoad,True)
+        loaded = imp.load_source(modulename,filename)
+        toLoad = eval("loaded."+classname)()
+        caller = GameCaller()
+        #caller.GameCall(toLoad,True)
+        toLoad.Go()
+        #Here be test
+        #GameThead = multiprocessing.Process(None,toLoad.Go,"Juego",(),{})
+        #GameThead.daemon = False #Nos aseguramos que Phyton no puede cerrar si el juego no ha cerrado
+        #GameThead.start()
+        #GameThead.join()
+        #Here be end test
+
+def LaunchGameFromFileMT(filename,classname,modulename):
+        nproc = multiprocessing.Process(None,LaunchGameFromFile,"Juego",(filename,classname,modulename),{})
+        nproc.daemon = False
+        nproc.start()
+        nproc.join()
