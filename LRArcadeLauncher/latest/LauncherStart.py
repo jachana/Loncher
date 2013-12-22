3c4494f8ceaed063992e562fb918db8f8f721c13
Jurgen Heysen <jheysen@gmail.com>
2013-12-22 00:35:46 -0300

    Don't stop the coding

---
+++ LRArcadeLauncher/latest/LauncherStart.py
@@ -0,0 +1,13 @@
+#Script de inicio del Launcher
+import tools
+def LauncherMain():
+	#Comenzamos el inicio de las cosas
+	#Primero obtenemos la lista de juegos
+	#TODO: Obtener lista
+	#Ahora ejecutamos scripts de extensiones (si, código arbitrario)
+	tools.runparts("./launcherinit.d/")
+	#Finalizamos con lanzar la interfaz
+	#TODO: Lanzar interfaz
+
+if __name__== "__main__":
+	LauncherMain():
