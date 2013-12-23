4d3eec58b51c89e2338573c327016d8f837aef84
Jurgen Heysen <jheysen@gmail.com>
2013-12-22 23:36:41 -0300

    Código para descubrir servicios. Se instalan dejandolos en la carpeta simplemente. Draft del servicio de HighScores (creado archivo que sigue estándar)

---
+++ LRArcadeLauncher/latest/services/HighScoreService.py
@@ -0,0 +1,10 @@
+#Servicio de Highscores
+
+#Metodo que crea un objeto que representa el servicio a partir de
+#un diccionario de argumentos y el codigo del juego
+def factory(args,code):
+	pass
+
+#Metodo que registra el servicio al momento de descubrirlo
+def register():
+	return "HighScoreService",factory
---
+++ LRArcadeLauncher/latest/servicesDiscovery.py
@@ -0,0 +1,21 @@
+#Script que se encarga de descubrir los servicios
+import os
+import imp
+
+def discover(path):
+        servlist = {}
+        oldpath = os.getcwd()
+        os.chdir(path)
+        for files in os.listdir("."):
+                if files.endswith(".py"):
+                        loaded = imp.load_source("Loaded",files)
+                        try:
+                                name,serverFactory=eval("loaded.register()")
+                                servlist[name]=serverFactory
+                        except Exception:
+                                pass
+        os.chdir(oldpath)
+        return servlist
+								
+if __name__ == "__main__":
+        print(discover("./services/"))
