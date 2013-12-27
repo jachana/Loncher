fb1479d121e4f33144ba6984882b65a207209ee3
Jurgen Heysen <jheysen@gmail.com>
2013-12-27 16:31:30 -0300

    Agregada interfaz de servicios como parámetro a la llamada, elliminado código que no se usa

--- LRArcadeLauncher/latest/ReflectionLoader.py
+++ LRArcadeLauncher/latest/ReflectionLoader.py
@@ -1,28 +1,19 @@
-from LRArcadeLauncher import GameCaller
 import imp
 import StartBase
 import multiprocessing
 import tools
 import os
 
-def LaunchGameFromFile(filename,classname,modulename):
+def LaunchGameFromFile(filename,classname,modulename,serviceinterface):
         """Usar metodo MT mejor."""
-		tools.runparts(os.path.append(os.path.dirname(os.path.abspath(filename)),"\\gameinit.d"))#22-12-2013: Permitimos al juego agregar código arbitrario de inicialización
+	tools.runparts(os.path.append(os.path.dirname(os.path.abspath(filename)),"\\gameinit.d"))#22-12-2013: Permitimos al juego agregar código arbitrario de inicialización
         loaded = imp.load_source(modulename,filename)
         toLoad = eval("loaded."+classname)()
-        #caller = GameCaller()
-        #caller.GameCall(toLoad,True)
-        toLoad.Go()
-        #Here be test
-        #GameThead = multiprocessing.Process(None,toLoad.Go,"Juego",(),{})
-        #GameThead.daemon = False #Nos aseguramos que Phyton no puede cerrar si el juego no ha cerrado
-        #GameThead.start()
-        #GameThead.join()
-        #Here be end test
+        toLoad.Go(serviceinterface)
 
-def LaunchGameFromFileMT(filename,classname,modulename="Loading"):
+def LaunchGameFromFileMT(filename,classname,modulename="Loading",serviceinterface):
         """Permite la carga de la clase classname en el archivo (path completo) filename con el nombre modulename (esto puede ser fijo)"""
-        nproc = multiprocessing.Process(None,LaunchGameFromFile,"Juego",(filename,classname,modulename),{})
+        nproc = multiprocessing.Process(None,LaunchGameFromFile,"Juego",(filename,classname,modulename,serviceinterface),{})
         nproc.daemon = False
         nproc.start()
         nproc.join()
