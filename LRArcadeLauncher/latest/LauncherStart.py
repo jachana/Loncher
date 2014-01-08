2f0747a1c0bafacb676de4eae9b24af22d8bb2db
Jurgen Heysen <jheysen@gmail.com>
2014-01-08 19:52:33 -0300

    Agregada imagen que faltaba. Se ha modificado script de inicio para que inicie por default con la GUI, usar -c/--console desde consola para usar la CLI

--- LRArcadeLauncher/latest/LauncherStart.py
+++ LRArcadeLauncher/latest/LauncherStart.py
@@ -5,6 +5,12 @@ import servicesDiscovery as SD
 import ArcadeConfig as AC
 import ArcadeBackendFacade as ABF
 import ArcadeCLI
+import argparse
+import Interfaz
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-c","--console", action="store_true", help="Uses CLI instead of GUI")
+args = parser.parse_args()
 
 def LauncherMain():
 	#Comenzamos el inicio de las cosas
@@ -21,7 +27,11 @@ def LauncherMain():
 	#Primero inicializamos el backend
 	fac = ABF.ArcadeBackendFacade(lista,services)
 	#TODO: poner la GUI a andar
-	gui = ArcadeCLI.ArcadeCLI(fac)
+	gui = None
+	if args.console:
+		gui = ArcadeCLI.ArcadeCLI(fac)
+	else:
+		gui = Interfaz.ArcadeGUI(fac)
 	gui.go()
 
 if __name__== "__main__":
