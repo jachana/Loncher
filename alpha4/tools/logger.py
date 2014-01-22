0c08fd68d74bdd9054df282d7eea1a19da335bbe
Jurgen Heysen <jheysen@gmail.com>
2014-01-22 15:45:01 -0300

    Corregidas impresiciones con path

--- alpha4/tools/logger.py
+++ alpha4/tools/logger.py
@@ -1,7 +1,7 @@
+import os
+import time
 
 global instance
-
-import time
 
 #Codigo desde http://stackoverflow.com/questions/6760685/creating-a-singleton-in-python
 class _Singleton(type):
@@ -19,11 +19,12 @@ class Logger(Singleton):
 	"""
 
 	def __init__(self, path, allowConsole = False, autoWriteback = True):
-		self._path = path
+		#self._path = path
 		self._useConsole = allowConsole
 		self._stamp = time.strftime("%d-%m-%y  %H%M")
 		self._buf = ""
 		self._writebackmode = autoWriteback
+		self._path = os.path.abspath(path+self._stamp+".log")
 
 	def save(self):
 		"""Metodo encargado de guardar el buffer de log en archivo.
@@ -31,14 +32,14 @@ class Logger(Singleton):
 
 		try:
 			#Abrir archivo en modo append
-			with open(self._path+self._stamp+".log",'a') as f:
+			with open(self._path,'a') as f:
 				#Escribir lo que este en buffer de escritura
 				f.write(self._buf)
 				#vaciar buffer
 				self._buf = ""
 		except IOError:
 			#Abrir archivo en modo write
-			with open(self._path+self._stamp+".log",'w') as f:
+			with open(self._path,'w') as f:
 				#Escribir lo que este en buffer de escritura
 				f.write(self._buf)
 				#vaciar buffer
