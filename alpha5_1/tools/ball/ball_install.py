cc95fc4b805445676521a9754f579042931cc210
Jurgen Heysen <jheysen@gmail.com>
2014-01-25 16:20:12 -0300

    Avances en lo que es la lógica principal de ball.
    Falta hacer la verificación de dependencias, así como definir exactamente BallInfo.
    Se utilizará modelo estrategy con apoyo de reflection para añadir nuevos tipos sin dolor.

--- alpha5_1/tools/ball/ball_install.py
+++ alpha5_1/tools/ball/ball_install.py
@@ -2,6 +2,7 @@
 
 import zipfile
 import tools.logger
+import ball_info
 
 global log_
 log_ = logger.get_logger()
@@ -15,18 +16,32 @@ def install_ball(path):
 	with zipfile.ZipFile(path,"r") as b:
 		b_file = b.open("BallInfo.xml")
 		#Ahora se verifican que existan las dependencias
-		if not check_dependencies(b_file):
+		b_info = ball_info.BallInfo(b_file)
+		if not check_dependencies(b_info):
 			#Faltan requisitos
 			log_.log("[BallInstall/install_ball] Missing requisites for ball "+path)
+			#Informamos que no se realizo instalacion
+			return False
 		#Si pasa el check de dependencias, procedemos
 		log_.log("[BallInstall/install_ball] Dependencies OK")
 		#TODO: Llamar a instalador del tipo
+		return_val = False
+		try:
+			imstr = "install."+str(b_info.get_balltype)
+			eval("import imstr")
+			return_val = eval("imstr.install(b)") #Solicitamos al instalador que haga la pega
+		except Exception:
+			#Algo salio mal, probablemente no existe el instalador de ese tipo
+			log_.log("[BallInstall/install_ball] Exception during install, aborted. Is balltype standard or is the installHandler installed?")
+			log_.log("Stack trace:")
+			log_.log(str(Exception.args))
 	#end_with
+	return return_val
 
 def check_dependencies(b_info):
 	"""Funcion que dado un objeto BallInfo, verifica que se tengan los pre-requisitos instalados. Retorna True si estan presentes
 	o False si no.
 	"""
 
-	pass
+	return True
 
