607a980efd479266bf77bc1fcbcb2f964b3f771f
Jurgen Heysen <jheysen@gmail.com>
2014-02-07 23:43:34 -0300

    Ejecución de comandos
    Ahora los comandos son buscados y ejecutados.
    Queda por hacer impresión de ayuda y algunos comandos.

--- latest/system/shell/commandlist.py
+++ latest/system/shell/commandlist.py
@@ -32,8 +32,46 @@ class CommandList:
         #Now that registry process is complete, we sort the list so command searches can be faster
         self._clist.sort()
 
+    def _bsearch(self, name, init_index, end_index):
+    	"""Performs a binary-search over the command list looking for the processor. returns a handle to the processor if found or None otherwise.
+    	"""
+
+    	#Base case: lenght is 1 or 0
+    	if init_index - end_index < 0:
+    		if self._clist[init_index][0] == name:
+    			return self._clist[init_index]
+    		else:
+    			return None
+    	#Base case: element at middle point
+    	if self._clist[middle][0] == name:
+    		return self._clist[middle]
+    	#Not in base case, recursive call
+    	middle = int((init_index + end_index)/2)
+    	if self._clist[middle][0] < name:
+    		#We need the right side list
+    		return self._bsearch(name,middle,end_index)
+    	if self._clist[middle][0] > name:
+    		#left side
+    		return self._bsearch(name,init_index,middle)
+
 	def command(self, cmd):
 		"""Interpreta un string como comando e intenta ejecutarlo.
 		"""
 
-		pass
+		scmd = str(cmd) #First, we meke sure we are dealing with strings
+		l_scmd = scmd.split(" ") #We make a split on whitespaces
+		args = "" #We reserve a buffer for arguments, we start by assuming it is empty
+		#Now we concatenate args (if any)
+		if len(l_scmd) > 1:
+			for i in range(1,len(l_scmd)+1):
+				args = args + l_scmd[i] + " "
+		#Now we have the args, and the command. We isolate it for code clarance purposes.
+		mcmd = l_scmd[0]
+		mcmd = mcmd.lower() #commands should be lowercase
+		#Now, we perform a binary-search over the command list
+		cproc = self._bsearch(mcmd,0,len(self._clist)) #We allocate a variable for referencing the command-processor
+		#Now, we execute
+		if cproc is not None:
+			cproc[1](args)
+		else:
+			pass #TODO: something here
